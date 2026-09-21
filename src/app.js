dced35009f2e93f4226146cb6bc67aa6805450d8
zapmaster100 <jpszewc@gmail.com>
2026-09-21 19:48:19 -0400

    CA-0921-54 build id

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import {CONFIG} from './config.js';
-const BUILD_ID='CA-0921-53';
+const BUILD_ID='CA-0921-54';
 
 const players=['Blue','Red','Yellow','White'].map((name,i)=>({name,cash:CONFIG.startCash,tokens:CONFIG.tokens,owned:[],i,bot:i!==2}));
 let turn=0,actions=CONFIG.actions,mode='acquire',selected=null,population=0,jobs=0,setupDraft=true,draftPick=0,pendingBuilding=null,roadSegments=0,actionStart=null,pendingCouncil=null;
